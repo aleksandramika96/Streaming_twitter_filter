034203765a6b22687177489d1d1371c42dcc278d
aleksandramika96 <61654839+aleksandramika96@users.noreply.github.com>
2021-11-21 16:30:38 +0100

    add some tips

--- flask/twatcher.py
+++ flask/twatcher.py
@@ -4,10 +4,18 @@ from tweet_storage_redis.tweet_store import TweetStore
 app = Flask(__name__)
 store = TweetStore()
 
+#decorator which tells the application which url should call the associated function
+#route decorator in Flask is used to bind url to a function; for example app.route('/contact') def contact () : ... , user visit http://localhost:5000/contact url, output contact() function will be rendered in browser
+
+# some example
+# @app.route('/hello/<name>')
+# def hello_name(name):
+#     return f'Hello {name}'
+
 @app.route('/')
 def index():
     tweets = store.tweets()
     return render_template('index.html', tweets=tweets)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True) #app.run(host, port, debug, option)
